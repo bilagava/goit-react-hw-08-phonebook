0048c1c31340a8c4283257b9bb389c364ef816d6
Yuriy Brovko <ysb.ptc@gmail.com>
2022-07-23 12:00:14 +0300

    Update index.jsx

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -16,20 +16,6 @@ export class App extends React.Component {
     filter: '',
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parse = JSON.parse(contacts);
-    if (parse) {
-      this.setState({ contacts: parse });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
   addContact = (name, number) => {
     const contact = {
       id: nanoid(),
